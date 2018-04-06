709e11604d49f39cd0c010696acac7e224636632
Vishnu Chandran <vchandran@amaris.com>
2018-04-06 17:29:58 +0530

    06-04-2018|5:29pm

---
+++ src/app/server-element/server-element.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit , Input } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css']
+})
+export class ServerElementComponent implements OnInit {
+
+  // srvElement is the alias name provided so that the it can be accessed outside.
+  // Internally still it would be called as Element.
+
+  @Input('srvElement') element : {type: string,name: string,content: string };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
