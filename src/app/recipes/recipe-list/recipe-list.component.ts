3143d420e3a65fca33b744361920ef3f8205ba82
Vishnu Chandran <vchandran@amaris.com>
2018-04-19 17:46:52 +0530

    19-04-2018-05:46pm

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,16 +8,25 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes : Recipe[] = [new Recipe("A test recipe",
+  recipes : Recipe[] = [new Recipe("Recipe 1",
   "Test recipe description",
-  "https://upload.wikimedia.org/wikipedia/commons/f/fd/Indonesian_Food.png")] ;
+  "https://upload.wikimedia.org/wikipedia/commons/f/fd/Indonesian_Food.png"),
+  new Recipe("Recipe2",
+  "Recipe2 Details",
+  "https://www.hadiah.me/blog/wp-content/uploads/2017/04/160222142607-indonesian-food-bebek-goreng-6558-1900px-super-169.jpg")] ;
 
 
   constructor() { }
 
   ngOnInit() {
     
+  }
+
+
+  onRecipeSelected(recipe:Recipe){
+      this.recipeWasSelected.emit(recipe);
   }
 
 }
