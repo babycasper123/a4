e487910420427148a98d8b880cfc9610f2affb26
Vishnu Chandran <vchandran@amaris.com>
2018-04-25 17:09:23 +0530

    25-04-18:05-08

---
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, ElementRef, Renderer2 , OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective {
+
+  constructor(private elRef:ElementRef , private renderer:Renderer2) { }
+
+  ngOnInit(){
+      this.renderer.setStyle(this.elRef.nativeElement,'backgroundColor','blue');
+  }
+
+}
